22ac879e27cfdbb6297cf58fdef1095086dd0732
root <root@raspberrypi.(none)>
2013-08-16 03:32:01 +0000

    August 15 - Added new light

--- code/lights.sh
+++ code/lights.sh
@@ -3,6 +3,17 @@ NAME=heyu
 OPTIONS=""
 USAGE=" * Usage: $NAME [LIGHT#] [on|off]"
 PID=$(pidof $NAME)
+function light1 {
+	if [ "$1" = "on" ] ; then
+		heyu fon K1
+		echo "Light 1 turned on."
+	elif [ "$1" = "off" ] ; then
+		heyu foff K1
+		echo "Light 1 turned off."
+	else
+		echo "$USAGE"
+	fi
+}
 function light2 {
 	if [ "$1" = "on" ] ; then
 		heyu fon K2
@@ -14,8 +25,10 @@ function light2 {
 		echo "$USAGE"
 	fi
 }
-if [ "$1" = "2" ]; then
-        light2 $2
+if [ "$1" = "1" ]; then
+       light1 $2
+elif [ "$1" = "2" ]; then
+	light2 $2
 else
         echo "$USAGE"
 fi
--- lighting.php
+++ lighting.php
@@ -8,7 +8,13 @@
 <div class='col one-third'>
 	<h3>Controls</h3>
 	<form method='post'>
-		<p>Bedside light:</p>
+		Desk light:
+		<div class='form-space'></div>
+		<button class='button half' name='light1on'>On</button>
+		<button class='button half' name='light1off'>Off</button>
+		<div class='form-space'></div>
+		Bedside light:
+		<div class='form-space'></div>
 		<button class='button half' name='light2on'>On</button>
 		<button class='button half' name='light2off'>Off</button>
 	</form>
@@ -17,13 +23,17 @@
 	<h3>Output</h3>
 	<?php
 		$DIR = "code";
+		if (isset($_POST['light1on'])) {
+			exec("$DIR/lights.sh 1 on",$output,$retval);
+		}
+		if (isset($_POST['light1off'])){
+			exec("$DIR/lights.sh 1 off",$output,$retval);
+		}
 		if (isset($_POST['light2on'])) {
 			exec("$DIR/lights.sh 2 on",$output,$retval);
 		}
 		if (isset($_POST['light2off'])){
 			exec("$DIR/lights.sh 2 off",$output,$retval);
-		}
-			else {
 		}
 		echo '<p>';
 		foreach ($output as $item) {
