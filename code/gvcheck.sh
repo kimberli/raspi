aac37cdef2e962af97e26fec1879bc166eeedd47
root <root@raspberrypi>
2013-09-09 16:46:44 -0700

    Changed GV check frequency

--- code/gvcheck.sh
+++ code/gvcheck.sh
@@ -4,8 +4,7 @@ NAME=gtextcommand
 DIR=/usr/bin
 
 #checks google voice approx. every 4-6 seconds (every minute, as set in the Pi user's crontab)
-for i in {1..10}
+for i in {1..12}
 	do
 		$DIR/gtextcommand
-		sleep 2
 	done
