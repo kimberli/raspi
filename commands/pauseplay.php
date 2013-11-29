20fa1556fe500373341f6b1044c555b2e7cf5ae2
kimberli <kimberlizhong@gmail.com>
2013-11-28 17:04:54 -0800

    Added NFC start music to pauseplay.php

--- commands/pauseplay.php
+++ commands/pauseplay.php
@@ -1,5 +1,11 @@
 <?php
-	exec("/var/www/code/pandora.sh pause",$output);
+	exec("/var/www/code/pandora.sh status 2>&1", $status, $retv);
+	if (sizeof($status) > 1) {
+		exec("/var/www/code/pandora.sh pause",$output);
+	}
+	else {
+		exec("/var/www/code/pandora.sh start",$output);
+	}
 	foreach ($output as $item) {
 		echo $item . "<br>";
 	}
