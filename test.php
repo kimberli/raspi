67f0a0483e19dc4395ce0bfc1a12ae8a9e9b5843
kimberli <kimberlizhong@gmail.com>
2014-01-18 19:23:44 -0800

    In progress UI changes

---
+++ test.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Kimberli's Raspberry Pi</title>
+		<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+		<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
+		<link rel='shortcut icon' href='css/favicon.ico'>
+		<link rel='stylesheet' href='css/style2.css'>
+		<link href='http://fonts.googleapis.com/css?family=Cherry+Swash:700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Nunito:300' rel='stylesheet' type='text/css'>
+		<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>
+	</head>
+	
+	<body id='home-page'>
+		<header>
+			<div class='content page-header'>
+				<h1>Welcome Home</h1>
+			</div>
+		</header>
+		<main>
+			<div class='content main'>
+				<div class='module' id='module-music'>
+					This is a music module
+				</div>
+				<div class='module' id='module-lighting'>
+					This is a lighting module
+				</div>
+				<div class='module' id='module-tasks'>
+					This is a tasks module
+				</div>
+				<div class='module' id='module-status'>
+					This is a status module
+				</div>
+			</div>
+		</main>
+		<footer>
+			<div class='content page-footer'>
+				This is the footer About kimberli's raspberry pi
+			</div>
+		</footer>
+	</body>
+</html>
