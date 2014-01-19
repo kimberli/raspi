d9d1f0abacb72292a7a8d53fe1c75f6e2db4f960
kimberli <kimberlizhong@gmail.com>
2014-01-18 23:49:00 -0800

    Most of CSS stuff done

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 	<head>
 		<title>Kimberli's Raspberry Pi</title>
 		<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
@@ -7,36 +7,48 @@
 		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
 		<link rel='shortcut icon' href='css/favicon.ico'>
 		<link rel='stylesheet' href='css/style2.css'>
-		<link href='http://fonts.googleapis.com/css?family=Cherry+Swash:700' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Nunito:300' rel='stylesheet' type='text/css'>
 		<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>
 	</head>
+	
+	<div class='preloaded-images'>
+	   <img src='css/bg-large-focus.jpg' height='1' width='1'/>
+	   <img src='css/bg-large-unfocus.jpg' height='1' width='1'/>
+	   <img src='css/bg-small-focus.jpg' height='1' width='1'/>
+	   <img src='css/bg-small-unfocus.jpg' height='1' width='1'/>
+	</div>
 	
 	<body id='home-page'>
 		<header>
 			<div class='content page-header'>
-				<h1>Welcome Home</h1>
+				<h1 class='page-title'>Welcome Home.</h1>
 			</div>
 		</header>
 		<main>
-			<div class='content main'>
-				<div class='module' id='module-music'>
-					This is a music module
+			<div class='content bvc'>
+				<div class='bevel tl tr'></div>
+				<div class='page-main'>
+					<div class='full-width-module' id='module-status'>
+						This is a status module
+					</div>
+					<div class='module' id='module-weather'>
+						This is a weather module
+					</div>
+					<div class='module' id='module-music'>
+						This is a music module
+					</div>
+					<div class='module' id='module-lighting'>
+						This is a lighting module
+					</div>
+					<div class='module' id='module-tasks'>
+						This is a tasks module
+					</div>
 				</div>
-				<div class='module' id='module-lighting'>
-					This is a lighting module
-				</div>
-				<div class='module' id='module-tasks'>
-					This is a tasks module
-				</div>
-				<div class='module' id='module-status'>
-					This is a status module
-				</div>
+				<div class='bevel bl br'></div>
 			</div>
 		</main>
 		<footer>
 			<div class='content page-footer'>
-				This is the footer About kimberli's raspberry pi
+				Designed by Kimberli Zhong. View this project on GitHub at <a href='www.github.com/kimberli/raspi'>www.github.com/kimberli/raspi</a>.
 			</div>
 		</footer>
 	</body>
